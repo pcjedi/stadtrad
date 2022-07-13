92d2c870b21ca0e8fed0bf30950c9a21cc3ae0aa
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-07-13 20:00:36 +0200

    Update run.py

--- crawler/run.py
+++ crawler/run.py
@@ -8,6 +8,7 @@ payload_dict = dict()
 with open("crawler/params.json") as f:
     for param in json.load(f):
         payload = stadtrad_payload(**param, session=stadtrad_session())
+        assert len(payload)<50, f"payload length is {len(payload)} at {param}"
         payload_dict.update({(p["position"]["lat"], p["position"]["lng"]): p for p in payload})
 
 pd.json_normalize(data=payload_dict.values()).to_csv(
