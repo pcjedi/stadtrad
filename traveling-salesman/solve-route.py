382b749502e51d3c496d930af1afb69df1cef9e0
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-07-20 18:03:56 +0000

    tsp

---
+++ traveling-salesman/solve-route.py
@@ -0,0 +1,61 @@
+from glob import glob
+import pandas as pd
+from scipy.spatial import distance_matrix
+import numpy as np
+from tqdm import tqdm
+import json
+import os
+from python_tsp.heuristics import solve_tsp_simulated_annealing
+from utils import get_duration
+
+
+df = pd.read_csv(sorted(glob("data/*"))[-1])
+df = df[df["position.lat"] > 53.4].reset_index(drop=1)
+
+positions = df[["position.lat", "position.lng"]]
+dm = distance_matrix(positions, positions)
+stations = {f"{lat},{lng}": i for i, (lat, lng) in positions.iterrows()}
+
+not_loaded = 1
+while not_loaded > 0:
+    values = [
+        json.load(open(d))[0]["legs"][0]["duration"]["value"]
+        / (
+            sum(
+                [
+                    (s - d) ** 2
+                    for s, d in zip(
+                        [float(v) for v in d.split("/")[-2].split(",")], [float(v) for v in d.split("/")[-1][:-5].split(",")]
+                    )
+                ]
+            )
+            ** (1 / 2)
+        )
+        for d in tqdm(glob("directions/*/*.json"))
+    ]
+    duration_matrix = dm * np.mean(values)
+
+    for d in tqdm(glob("directions/*/*.json")):
+        origin = d.split("/")[-2]
+        destination = d.split("/")[-1][:-5]
+        duration = json.load(open(d))[0]["legs"][0]["duration"]["value"]
+        try:
+            duration_matrix[stations[origin], stations[destination]] = duration
+            if not os.path.exists(f"directions/{destination}/{origin}.json"):
+                duration_matrix[stations[destination], stations[origin]] = duration
+        except KeyError:
+            pass
+
+    permutation2, distance2 = solve_tsp_simulated_annealing(
+        distance_matrix=duration_matrix,
+    )
+
+    not_loaded = 0
+    for o, d in zip(np.roll(permutation2, 1), permutation2):
+        origin = positions.iloc[o].agg(lambda cols: ",".join([str(c) for c in cols]))
+        destination = positions.iloc[d].agg(lambda cols: ",".join([str(c) for c in cols]))
+        if not os.path.exists(f"directions/{origin}/{destination}.json"):
+            not_loaded += 1
+            get_duration(origin, destination, allow_reverse=False)
+
+print(permutation2)
---
+++ traveling-salesman/utils.py
@@ -0,0 +1,41 @@
+import os
+import googlemaps
+import json
+
+gmaps = googlemaps.Client(key=os.getenv("GMAPSKEY"))
+
+
+def get_duration(origin, destination, mode="bicycling", units="metric", allow_reverse=True):
+    folderpath = f"directions/{origin}"
+    filepath = f"{folderpath}/{destination}.json"
+    if os.path.exists(filepath):
+        with open(filepath) as f:
+            direction_result = json.load(f)
+    elif allow_reverse:
+        return get_duration(origin=destination, destination=origin, mode=mode, units=units, allow_reverse=False)
+    else:
+        direction_result = gmaps.directions(
+            origin=origin,
+            destination=destination,
+            mode=mode,
+            units=units,
+        )
+        os.makedirs(folderpath, exist_ok=True)
+        with open(filepath, "w") as f:
+            json.dump(
+                obj=direction_result,
+                fp=f,
+                indent=2,
+            )
+    return direction_result[0]["legs"][0]["duration"]["value"]
+
+
+def duration_available(origin, destination, allow_reverse=True):
+    folderpath = f"directions/{origin}"
+    filepath = f"{folderpath}/{destination}.json"
+    if os.path.exists(filepath):
+        return True
+    elif allow_reverse:
+        return duration_available(origin=destination, destination=origin, allow_reverse=False)
+    else:
+        return False
