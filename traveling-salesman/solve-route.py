84534567a7291b996dbef86ed3db7865f851da1f
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-07-20 18:32:36 +0000

    debug

--- traveling-salesman/solve-route.py
+++ traveling-salesman/solve-route.py
@@ -1,22 +1,25 @@
 from glob import glob
 import pandas as pd
-from scipy.spatial import distance_matrix
+from python_tsp.distances import great_circle_distance_matrix
 import numpy as np
 import json
 import os
 from python_tsp.heuristics import solve_tsp_simulated_annealing
 from utils import get_duration
+import datetime
+import googlemaps
 
 
 df = pd.read_csv(sorted(glob("data/*"))[-1])
 df = df[df["position.lat"] > 53.4].reset_index(drop=1)
 
 positions = df[["position.lat", "position.lng"]]
-dm = distance_matrix(positions, positions)
+dm = great_circle_distance_matrix(positions, positions)
 stations = {f"{lat},{lng}": i for i, (lat, lng) in positions.iterrows()}
 
 not_loaded = 1
-while not_loaded > 0:
+start = datetime.datetime.now()
+while not_loaded > 0 and (datetime.datetime.now() - start).seconds < 3600 * 5:
     values = [
         json.load(open(d))[0]["legs"][0]["duration"]["value"]
         / (
@@ -55,6 +58,11 @@ while not_loaded > 0:
         destination = positions.iloc[d].agg(lambda cols: ",".join([str(c) for c in cols]))
         if not os.path.exists(f"directions/{origin}/{destination}.json"):
             not_loaded += 1
-            get_duration(origin, destination, allow_reverse=False)
+            try:
+                get_duration(origin, destination, allow_reverse=False)
+            except googlemaps.exceptions.Timeout:
+                not_loaded = 0
+                print("breaking ..")
+                break
 
 print(permutation2)
